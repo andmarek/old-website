6a6ac76c18c74f6e1b8b72858b7022bf67c9d852
Andrew <andwmarek@gmail.com>
2022-05-08 16:09:13 -0500

    Finally get tailwind to recompile successfully..

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
@@ -11,16 +10,17 @@ module.exports = {
         'secondary': '#9191A4',
         'badge': '#3F3F51',
         'input-border': '#565666',
-        'input': '#2A2A35'
+        'input': '#2A2A35',
       },
       fontFamily: {
         'poppins': ["'Poppins'", 'sans-serif']
       }
     },
   },
-  variants: {
-    extend: {},
-    fill: ['hover', 'focus']
-  },
-
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
