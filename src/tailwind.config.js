0974d9705a0de1850b99f698f296344aa86b0e67
Andrew <andwmarek@gmail.com>
2022-11-19 22:14:10 -0600

    nice updates :)

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -5,15 +5,21 @@ module.exports = {
       colors: {
         'body': '#282828',
         'selected-text': '#fe8019',
-        'theme': '#689d6a',
+        'theme': '#8ec07c',
+        'theme-dim': '#689d6a',
         'nav': '#404053',
         'primary-dim': '#b16286', // purple
         'primary': '#d3869b', // purple
-        'secondary': '#458588', //blu-ish
+        'secondary-dim': '#458588', //blu-ish
+        'secondary': '#83a598', //blu-ish
         'tertiary-bright': '#fabd2f', // yellow orangeish
         'tertiary-dim': '#d79921', // yellow orangish
         'badge': '#3F3F51',
+        'badge-selected': '#504945',
+        'gruvbox-gray-dim': '#d5c4a1',
+        'gruvbox-gray': '#ebdbb2',
         'input-border': '#565666',
+        'badge-dim': '#3c3836',
         'input': '#2A2A35',
       },
       fontFamily: {
